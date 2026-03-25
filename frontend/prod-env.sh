3c79b322dfe92459478d8fc6b9e6da620bdf51b5
Tuomo Smolander <58043704+tsmoland@users.noreply.github.com>
2026-03-25 12:01:37 +0200

    Trying to fix installation problems

--- frontend/prod-env.sh
+++ frontend/prod-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export DEV="false"
-export VITE_BACKEND_PROD_API_URL="https://api.maastonkulkukelpoisuus-dev.fi"
+export VITE_BACKEND_PROD_API_URL="https://backend-harvesterseasons.2.rahtiapp.fi"
+# export VITE_BACKEND_PROD_API_URL="https://api.maastonkulkukelpoisuus-dev.fi"

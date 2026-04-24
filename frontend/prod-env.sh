13491eebd50220e6db073a7719cd3a89eb3cb46d
Tuomo Smolander <58043704+tsmoland@users.noreply.github.com>
2026-04-24 11:10:17 +0300

    Change backend url to dev-version

--- frontend/prod-env.sh
+++ frontend/prod-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export DEV="false"
-export VITE_BACKEND_PROD_API_URL="https://api-trafficability.2.rahtiapp.fi/"
+export VITE_BACKEND_PROD_API_URL="https://api-trafficability-dev.2.rahtiapp.fi/"
+# export VITE_BACKEND_PROD_API_URL="https://api-trafficability.2.rahtiapp.fi/"
 # export VITE_BACKEND_PROD_API_URL="https://api.maastonkulkukelpoisuus-dev.fi"
